ee3c3974d3840c16d335f317d69a49edd7052e48
admin <drjemo@outlook.com>
2023-09-06 21:57:09 +0800

    显示商品信息

--- ShopVue/shopvue/src/components/ProductList.vue
+++ ShopVue/shopvue/src/components/ProductList.vue
@@ -1,5 +1,27 @@
 <template>
-    <h2>ProductList</h2>
+    <h2>商品列表</h2>
+
+    <div class="container">
+        <template v-for="product in ProductList.Products" :key="product.id">  
+            <dvi class="product">   
+                <a @click="detail(product.id)">
+                    <img class="media-img" :src="product.imageUrl">
+                </a>
+
+                <div class="media-body">
+                    <a @click="detail(product.id)">
+                        <h3>{{ product.titile }}</h3>
+                    </a>
+
+                    <a>{{ product.description }}</a>
+
+                    <template v-if="product.variants!=null&&product.variants.length>0">
+                        <h5 class="price"> {{getMinPrice(product)}} </h5>
+                    </template>
+                </div>
+            </dvi>
+        </template>
+    </div>
 </template>
 
 <script setup>
@@ -12,8 +34,60 @@ const ProductList = useProductList(pinia)
 onMounted(async () => {
     await ProductList.getAllProduct()
 })
+
+function detail(id) {
+
+}
+
+const getMinPrice = function GerPriceText(product) {
+    var variants = product.variants
+    if (variants.length == 0){
+        return "";
+    }
+    else if (variants.length == 1){
+        return "Y" + variants[0].price;
+    }
+}
 </script>
 
-<style> 
+<style scoped> 
+.container .product{
+    width: 100%;
+}
 
+.media-img{
+    margin: 20px;
+    max-height: 200px;
+    max-width: 200px;
+    border-radius: 6px;
+    margin-bottom: 10px;
+
+    transition: transform .2s;
+    flex: 1;
+}
+
+a:hover{
+    color:purple;
+    text-decoration: underline;
+}
+
+.media-img:hover {
+    transform: scale(1.1);
+}
+
+.product{
+    width: 200px;
+    text-align: center;
+    display: flex;
+}
+
+.media-body{
+    text-align: left;
+    flex: 1;
+    margin-left: 20px;
+}
+
+.price{
+    color: green;
+}
 </style>
